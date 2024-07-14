375a5d45acda9b564b5de3f275bf62b5fabfb05f
Rahul Rana <163443364+student-gehu@users.noreply.github.com>
2024-07-14 09:42:42 +0530

    Add files via upload

---
+++ WalkingTheDOM.js
@@ -0,0 +1,10 @@
+// window.console.log(window)
+// console.log(document.body)         //search in html by using DOM
+
+
+// document.body.style.background="red"         //not use permently but works good
+
+console.log(document.body.firstChild)
+console.log(document.body.lastChild)
+let arr=Array.from(document.body.childNodes)
+console.log(document.body.firstChild)
